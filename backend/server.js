58d504eeb9313930d5e11ec751cd3b4713da1c61
ayyildizg <150834252+ayyildizg@users.noreply.github.com>
2026-03-14 01:55:24 +0300

    fix render port

--- backend/server.js
+++ backend/server.js
@@ -43,6 +43,8 @@ app.post("/appointments", (req, res) => {
   });
 });
 
-app.listen(3001, () => {
-  console.log("Server started on port 3001");
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log("Server started");
 });
